a0a092927547b67e3b622e37c94cd07e8673cdaa
Sigrid <sigrid.c@hotmail.fr>
2018-11-29 19:54:23 +0100

    Add sphere player in first scene, add player moves

---
+++ Ted/Assets/Scripts/PlayerMoves.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoves : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("z")) {
+            gameObject.GetComponent<Transform>().Translate(Vector3.forward * moveSpeed);
+           }
+	}
+}
